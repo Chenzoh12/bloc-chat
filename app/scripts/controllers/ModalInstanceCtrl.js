b2fb8bb481d079bfb4dfc992b5cdab0d90c9cb19
Vincent Scotti <scottivincentj@gmail.com>
2017-07-31 23:17:50 -0400

    End of ng-cp-3. Works as described.

--- app/scripts/controllers/ModalInstanceCtrl.js
+++ app/scripts/controllers/ModalInstanceCtrl.js
@@ -1,16 +1,26 @@
-(function() {
-  function ModalInstanceCtrl($scope, $uibModalInstance) {
+angular.module('blocChat')
+  .controller('ModalInstanceCtrl', function($scope, $uibModalInstance){
     var $ctrl = this;
+    // Get a reference to the database service make available to whole controller
+    var database = firebase.database();
 
-    $ctrl.ok = function () {
+    var newRoom = {
+      newName: 'Room'
+    };
+    // Controller submit function
+    $ctrl.submit = function createNewRoom(name) {
+      // A new room object
+      newRoom.newName = name;
+      // Write the new room's name in the room list
+      var updates = {};
+      updates[ '/rooms/' + name ] = name;
+
+      $uibModalInstance.close();
+      return firebase.database().ref().update(updates);
+    };
+
+    $ctrl.cancel = function (result) {
       $uibModalInstance.close();
     };
 
-    $ctrl.cancel = function () {
-      $uibModalInstance.dismiss('cancel');
-    }
-  }
-  angular
-    .module('blocChat')
-    .controller('ModalInstanceCtrl', ['$scope', '$uibModalInstance', ModalInstanceCtrl]);
-})();
+});
