1b2b77af09b0dbc93879b205b9e1070ea15dee97
Vincent Scotti <scottivincentj@gmail.com>
2017-08-16 23:21:44 -0400

    Refactored cp 3 so I could 1. have better practices 2. regain myself. Still not seeing the element go all the way down.

--- app/scripts/controllers/RoomCtrl.js
+++ app/scripts/controllers/RoomCtrl.js
@@ -1,9 +1,18 @@
-(function() {
-  function RoomCtrl(Room) {
-    this.roomList = Room.all;
-  }
+angular.module('blocChat')
+    .controller('RoomCtrl', function(Room, $uibModal, $log, $document){
+        var roomControl = this;
+        roomControl.animationsEnabled = true;
 
-  angular
-    .module('blocChat')
-    .controller('RoomCtrl', ['Room', RoomCtrl]);
-})();
+        roomControl.open = function () {
+          var modalInstance = $uibModal.open({
+            animation: roomControl.animationsEnabled,
+            ariaLabelledBy: 'modal-title',
+            ariaDescribedBy: 'modal-body',
+            templateUrl: 'templates/newRoomModal.html',
+            controller: 'ModalInstanceCtrl',
+            controllerAs: 'modalControl',
+          });
+        };
+
+        this.roomList = Room.all;
+    });
